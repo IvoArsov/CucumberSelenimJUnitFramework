ff421491c148ff10d0b062edfa7096b5da73b6e6
IvoArsov <arsov.ivaylo@gmail.com>
2021-03-01 21:17:15 +0200

    add some Selenium code

--- src/test/java/CucumberFramework/steps/LoginSteps.java
+++ src/test/java/CucumberFramework/steps/LoginSteps.java
@@ -1,68 +1,67 @@
 package CucumberFramework.steps;
 
+import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class LoginSteps {
+
+	WebDriver driver;
+
+	@Before()
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\User\\Desktop\\CUCUMBER JAVA\\CucumberFramework\\src\\test\\java\\CucumberFramework\\resources\\chromedriver.exe");
+		this.driver = new ChromeDriver();;
+		this.driver.manage().window().maximize();
+		this.driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+	}
+
 	@Given("^User navigates to stackoverflow website$")
 	public void user_navigates_to_stackoverflow_website() throws Throwable {
-		System.out.println("user_navigates_to_stackoverflow_website");
+		this.driver.get("https://stackoverflow.com/");
 	}
 
 	@Given("^User clicks on login button$")
 	public void user_clicks_on_login_button() throws Throwable {
-		System.out.println("user_clicks_on_login_button");
+		this.driver.findElement(By.xpath("//a[contains(text(),'Log in')]")).click();
 	}
 
 	@Given("^User enters a valid username$")
 	public void user_enters_a_valid_username() throws Throwable {
-		System.out.println("user_enters_a_valid_username");
+		//Thread.sleep(3000);
+		this.driver.findElement(By.id("email")).sendKeys("arsov.ivaylo@gmail.com");
 	}
 
 	@Given("^User enters a valid password$")
 	public void user_enters_a_valid_password() throws Throwable {
-		System.out.println("user_enters_a_valid_password");
+		this.driver.findElement(By.id("password")).sendKeys("arsov321@");
 	}
 
 	@When("^User clicks on the login button$")
 	public void user_clicks_on_the_login_button() throws Throwable {
-		System.out.println("user_clicks_on_the_login_button");
+		this.driver.findElement(By.id("submit-button")).click();
 	}
 
 	@Then("^User should be taken to the successful login page$")
 	public void user_should_be_taken_to_the_successful_login_page() throws Throwable {
-		System.out.println("user_should_be_taken_to_the_successful_login_page");
+		Thread.sleep(3000);
+		WebElement askQuestionBtn = this.driver.findElement(By.xpath("//a[contains(text(), 'Ask Question')]"));
+		Assert.assertEquals(true, askQuestionBtn.isDisplayed());
 	}
-	/*====================================================================================*/
-	@Given("^User navigates to stackoverflow website(\\d+)$")
-	public void user_navigates_to_stackoverflow_website(int arg1) throws Throwable {
-		System.out.println("user_navigates_to_stackoverflow_website2");
-	}
-
-	@Given("^User clicks on login button(\\d+)$")
-	public void user_clicks_on_login_button(int arg1) throws Throwable {
-		System.out.println("user_clicks_on_login_button2");
-	}
-
-	@Given("^User enters a valid username(\\d+)$")
-	public void user_enters_a_valid_username(int arg1) throws Throwable {
-		System.out.println("user_enters_a_valid_username2");
-	}
-
-	@Given("^User enters a valid password(\\d+)$")
-	public void user_enters_a_valid_password(int arg1) throws Throwable {
-		System.out.println("user_enters_a_valid_password2");
-	}
-
-	@When("^User clicks on the login button(\\d+)$")
-	public void user_clicks_on_the_login_button(int arg1) throws Throwable {
-		System.out.println("user_clicks_on_the_login_button2");
-	}
-
-	@Then("^User should be taken to the successful login page(\\d+)$")
-	public void user_should_be_taken_to_the_successful_login_page(int arg1) throws Throwable {
-		System.out.println("user_should_be_taken_to_the_successful_login_page2");
+	
+	@After()
+	public void cleanUp() {
+		this.driver.quit();
 	}
 }
--- target/cucumber/report.js
+++ target/cucumber/report.js
@@ -6,6 +6,10 @@ formatter.feature({
   "id": "login-into-account",
   "keyword": "Feature"
 });
+formatter.before({
+  "duration": 1821145400,
+  "status": "passed"
+});
 formatter.scenario({
   "line": 4,
   "name": "Login into account with correct credentials",
@@ -48,166 +52,46 @@ formatter.match({
   "location": "LoginSteps.user_navigates_to_stackoverflow_website()"
 });
 formatter.result({
-  "duration": 185277700,
+  "duration": 3388126800,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginSteps.user_clicks_on_login_button()"
 });
 formatter.result({
-  "duration": 77200,
+  "duration": 380371300,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginSteps.user_enters_a_valid_username()"
 });
 formatter.result({
-  "duration": 86200,
+  "duration": 88552600,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginSteps.user_enters_a_valid_password()"
 });
 formatter.result({
-  "duration": 77600,
+  "duration": 64928500,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginSteps.user_clicks_on_the_login_button()"
 });
 formatter.result({
-  "duration": 80900,
+  "duration": 44764000,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginSteps.user_should_be_taken_to_the_successful_login_page()"
 });
 formatter.result({
-  "duration": 89400,
+  "duration": 3055241600,
   "status": "passed"
 });
-formatter.uri("Login2.feature");
-formatter.feature({
-  "line": 1,
-  "name": "Login into account",
-  "description": "Existing user should be able to login to account using correct credentials",
-  "id": "login-into-account",
-  "keyword": "Feature"
-});
-formatter.scenario({
-  "line": 4,
-  "name": "Login into account with correct credentials2",
-  "description": "",
-  "id": "login-into-account;login-into-account-with-correct-credentials2",
-  "type": "scenario",
-  "keyword": "Scenario"
-});
-formatter.step({
-  "line": 5,
-  "name": "User navigates to stackoverflow website2",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 6,
-  "name": "User clicks on login button2",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 7,
-  "name": "User enters a valid username2",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 8,
-  "name": "User enters a valid password2",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 9,
-  "name": "User clicks on the login button2",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 10,
-  "name": "User should be taken to the successful login page2",
-  "keyword": "Then "
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "2",
-      "offset": 39
-    }
-  ],
-  "location": "LoginSteps.user_navigates_to_stackoverflow_website(int)"
-});
-formatter.result({
-  "duration": 1671600,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "2",
-      "offset": 27
-    }
-  ],
-  "location": "LoginSteps.user_clicks_on_login_button(int)"
-});
-formatter.result({
-  "duration": 127400,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "2",
-      "offset": 28
-    }
-  ],
-  "location": "LoginSteps.user_enters_a_valid_username(int)"
-});
-formatter.result({
-  "duration": 164800,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "2",
-      "offset": 28
-    }
-  ],
-  "location": "LoginSteps.user_enters_a_valid_password(int)"
-});
-formatter.result({
-  "duration": 96900,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "2",
-      "offset": 31
-    }
-  ],
-  "location": "LoginSteps.user_clicks_on_the_login_button(int)"
-});
-formatter.result({
-  "duration": 96800,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "2",
-      "offset": 49
-    }
-  ],
-  "location": "LoginSteps.user_should_be_taken_to_the_successful_login_page(int)"
-});
-formatter.result({
-  "duration": 108500,
+formatter.after({
+  "duration": 630024500,
   "status": "passed"
 });
 });
